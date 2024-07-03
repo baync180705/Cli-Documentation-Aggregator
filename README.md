3f59aed5e6cfe8f9779154beb30711a3fc0a349c
Budhayan <budhayanc2005@gmail.com>
2024-07-03 13:59:48 +0530

    init: project initialization

---
+++ README.md
@@ -0,0 +1,10 @@
+<h3>Steps to follow: </h3>
+<li>
+    <ul>clone this repo in your local system</ul>
+    <ul>cd to the root directory</ul>
+    <ul>Activate the virtual environment : venv/bin/activate for macOS and linux, venv\Scripts\activate for windows (DO THIS STEP ONLY IF YOU ARE WORKING ON PYTHON)</ul>
+    <ul>pip install -r requirements.txt </ul>
+</li>
+<br>
+<h2>Make sure to add your python dependencies in requirements.txt each time a new library is installed via pip install.</h2>
+#Label your code with comments to increase readability
---
+++ src/scrappers/proxy_ip.py
@@ -0,0 +1,36 @@
+#importing packages
+import requests
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv() #load the env into python's os env
+
+FETCH_PROXY_URL = os.getenv('FETCH_PROXY_URL') #api endpoint for fetching proxy ips
+PATH = os.path.join(os.path.dirname(__file__),'data/free-proxy-list.txt') #file location to store all ip addresses 
+
+proxyList = []
+
+data = requests.get(FETCH_PROXY_URL)
+
+#generates free-proxy-list.txt file
+def generateFile():
+    with open(PATH,'w') as file:
+            file.write(data.text)
+
+#initializes proxyList[]
+def createProxyList():
+    with open(PATH) as file:
+        for index in range(0,20):
+            line = file.readline().split('\n')[0]
+            proxyList.append(line)
+
+    
+    return(proxyList)
+
+#returns a random ip address from proxyList
+def randomProxyPicker():
+    generateFile()
+    proxies = createProxyList()
+    return random.choice(proxies)
+
