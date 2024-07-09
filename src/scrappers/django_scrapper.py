51b4136235cc05cdfe28aaf1cb90c0e9c46e896f
Budhayan <budhayanc2005@gmail.com>
2024-07-10 02:30:24 +0530

    refact: code revamped

--- src/scrappers/django_scrapper.py
+++ src/scrappers/django_scrapper.py
@@ -1,25 +1,25 @@
+#importing packages
 import os
 import requests
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import json
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from proxy_ip import randomProxyPicker
 
-load_dotenv()
+load_dotenv() #load the env into python's os env
 
-keyword = 'models'
+keyword = 'templates' #keyword entered
 
+#initialized proxies and paths
 FETCH_DJANGO_SEARCH = os.getenv('FETCH_DJANGO_SEARCH')
 HOME_PATH = os.path.join(os.path.dirname(__file__),'data/django-search.html')
 FILE_PATH = os.path.join(os.path.dirname(__file__),f'data/django-{keyword.lower()}.html')
 PROXY = {
     'http': f'{randomProxyPicker()}'
 }
+AI_MODEL_SERVER = os.getenv('AI_MODEL_SERVER')
 
-model_name = 'gpt2'
-model = GPT2LMHeadModel.from_pretrained(model_name)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-
+# Fetches the react search page
 def djangoDocsSearchedPageFetcher():
     data = requests.get(FETCH_DJANGO_SEARCH+keyword, proxies=PROXY)
     with open(HOME_PATH,'w') as file:
@@ -27,6 +27,7 @@ def djangoDocsSearchedPageFetcher():
 
 djangoDocsSearchedPageFetcher()
 
+#fetches the relevant url containint the keyword
 def fetchRelevantUrl():
     with open(HOME_PATH) as f:
         django_doc = f.read()
@@ -43,6 +44,7 @@ def fetchRelevantUrl():
         return 'ERROR'
     
 
+#get the webpage of the fetched relevant url and catches error if keyword not found
 def fetchRelevantPage():
     url = fetchRelevantUrl()
 
@@ -51,46 +53,40 @@ def fetchRelevantPage():
         with open(FILE_PATH,'w') as f:
             f.write(data.text)
     else:
-        print(f'ERROR: Keyword {keyword} not found')
+        print(f'ERROR: keyword: {keyword} not found. Please try with a different keyword.') #catches error if keyword is not found
 
 fetchRelevantPage()
 
+#generates the paragraph list conatining all the scraped data
 def createParagraphsList():
-    paragraphs = []
+    paragraphs = [] #empty list initialized
     with open(FILE_PATH) as f:
         django_page = f.read()
 
     soupe = BeautifulSoup(django_page, 'html.parser')
-    elements = soupe.find_all(['p','pre'])
+    elements = soupe.find_all(['p','pre']) 
 
     for element in elements:
         if(element.name == 'p'):
             cleaned_text = ' '.join(element.get_text().replace('\n', ' ').split())
             if(cleaned_text != 'The web framework for perfectionists with deadlines.'):
-                paragraphs.append(cleaned_text)
+                paragraphs.append(cleaned_text) #paragraphs appended
         
         if(element.name == 'pre'):
             code = ' '.join(['This is a sample code:', element.get_text()])
-            paragraphs.append(code)
+            paragraphs.append(code) #codes appended
         
     return paragraphs
 
 
-def generate_article(sentences):
-    prompt = "\n".join(sentences)[:1024]
-
-    
-    inputs = tokenizer.encode(prompt, return_tensors='pt')
-    
-    outputs = model.generate(inputs, max_length=1024, num_return_sequences=1, no_repeat_ngram_size=2, early_stopping=True)
-    
-    article = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return article
-
-
+#Shows the result if keyword exists
 if(os.path.exists(FILE_PATH)):
     paragraphs = createParagraphsList()
-    article = generate_article(paragraphs).replace('. ','.\n').replace('<','\n<').replace(': ',':\n')
-
-    with open(os.path.join(os.path.dirname(__file__),f'data/{keyword.lower()}.txt'),'w') as f:
-        f.write(article)
+    try:
+        article = (requests.post(AI_MODEL_SERVER, headers = {'Content-Type':'application/json'}, data=json.dumps(paragraphs))).text #throws paragraph to an ai model for documentation
+    except:
+        article = '. '.join(paragraphs) #raw data if the ai model apis do not work
+    finally:
+        article = article.replace('. ','.\n').replace('<','\n<').replace('>','>\n').replace(': ',':\n') #formats the data
+        with open(os.path.join(os.path.dirname(__file__),f'data/{keyword.lower()}.txt'),'w') as f:
+            f.write(article)
